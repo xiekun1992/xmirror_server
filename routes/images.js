c94687eacfb16f03dc76a207353fe9c3b092b9f7
xiekun1992 <840914927@qq.com>
2016-07-18 23:57:02 +0800

    '修改图片上传的代码'

--- routes/images.js
+++ routes/images.js
@@ -5,6 +5,19 @@ var formidable = require('formidable');
 router.post('/',function(req,res){
 	var type=req.headers['content-type']||'';
 	if(0==type.indexOf('multipart/form-data')){
+		var form=new formidable.IncomingForm();
+		form.on('field',function(field,value){
+			console.log(field,value);
+		});
+		form.on('file',function(name,file){
+			console.log(name,file);
+		});
+		form.on('end',function(){
+			console.log('received file');
+			res.statusCode=200;
+			res.send('OK: file uploaded');
+		});
+		form.parse(req);
 
 	}else{
 		res.statusCode=400;
