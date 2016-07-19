7347c3f9b315b59a492c4b4dfd7250ac9adc94d0
xiekun1992 <840914927@qq.com>
2016-07-19 23:54:53 +0800

    '完成图片上传功能'

--- routes/images.js
+++ routes/images.js
@@ -1,27 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var formidable = require('formidable');
+var multiparty = require('multiparty');
+var fs = require('fs');
 
 router.post('/',function(req,res){
 	var type=req.headers['content-type']||'';
 	if(0==type.indexOf('multipart/form-data')){
-		var form=new formidable.IncomingForm();
-		form.on('field',function(field,value){
-			console.log(field,value);
-		});
-		form.on('file',function(name,file){
-			console.log(name,file);
-		});
-		form.on('end',function(){
+		var form=new multiparty.Form({uploadDir:'public/uploads/'});
+		form.parse(req,function(err,fields,files){
+			var filesTmp = JSON.stringify(files,null,2);
+			if(err){
+				console.log('parse error: ' + err);
+			} else {
+				console.log('parse files: ' + filesTmp);
+				var inputFile = files.img[0];
+				var uploadedPath = inputFile.path;
+				var dstPath = 'public/uploads/' + Date.now() + '_' +inputFile.originalFilename;
+				//重命名为真实文件名
+				fs.rename(uploadedPath, dstPath, function(err) {
+					if(err){
+						console.log('rename error: ' + err);
+					} else {
+						console.log('rename ok');
+					}
+				});
+			}
 			console.log('received file');
-			res.statusCode=200;
-			res.send('OK: file uploaded');
+			res.status(200).json({msg:'file uploaded'});
 		});
-		form.parse(req);
 
 	}else{
-		res.statusCode=400;
-		res.send('Bad Request: expecting multipart/form-data');
+		res.status(400).json({msg:'expecting multipart/form-data'});
 	}
 });
 
