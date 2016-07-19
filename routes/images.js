d0e48f5d4abfba4091bc073c2e607f06cbb4e648
xiekun1992 <840914927@qq.com>
2016-07-20 01:20:21 +0800

    '增加上传图片获取进度的功能'

--- routes/images.js
+++ routes/images.js
@@ -1,37 +1,55 @@
 var express = require('express');
 var router = express.Router();
-var multiparty = require('multiparty');
 var fs = require('fs');
+var formidable = require('formidable');
+var uploadProgress={};
 
 router.post('/',function(req,res){
 	var type=req.headers['content-type']||'';
 	if(0==type.indexOf('multipart/form-data')){
-		var form=new multiparty.Form({uploadDir:'public/uploads/'});
-		form.parse(req,function(err,fields,files){
+		var form = new formidable.IncomingForm();
+		form.encoding='utf8';
+		form.maxFieldsSize=10*1024*1024;
+		var uploadId=req.query.uploadId;
+		uploadProgress[uploadId]=0;
+		form.on('progress', function(bytesReceived, bytesExpected){
+			uploadProgress[uploadId] = Math.floor(bytesReceived / bytesExpected * 100);
+			console.log(uploadProgress[uploadId],uploadId);
+			console.log('progress');
+		});
+		form.parse(req, function(err, fields, files){
+			console.log(fields);
+			console.log(files);
 			var filesTmp = JSON.stringify(files,null,2);
 			if(err){
 				console.log('parse error: ' + err);
 			} else {
 				console.log('parse files: ' + filesTmp);
-				var inputFile = files.img[0];
+				var inputFile = files.img;
 				var uploadedPath = inputFile.path;
-				var dstPath = 'public/uploads/' + Date.now() + '_' +inputFile.originalFilename;
-				//重命名为真实文件名
-				fs.rename(uploadedPath, dstPath, function(err) {
-					if(err){
-						console.log('rename error: ' + err);
-					} else {
-						console.log('rename ok');
-					}
+				var dstPath = 'public/uploads/' + Date.now() + '_' +inputFile.name;
+				var readStream=fs.createReadStream(uploadedPath);
+				var writeStream=fs.createWriteStream(dstPath);
+				readStream.pipe(writeStream);
+				readStream.on('close',function(){
+					fs.unlink(uploadedPath);
 				});
 			}
-			console.log('received file');
-			res.status(200).json({msg:'file uploaded'});
+			res.status(200).json({code:1001,msg:'file uploaded'});
 		});
 
 	}else{
-		res.status(400).json({msg:'expecting multipart/form-data'});
+		res.status(400).json({code:1002,msg:'expecting multipart/form-data'});
 	}
 });
+router.get('/:uploadId/progress',function(req,res){
+	var progress=uploadProgress[req.params.uploadId];
+	if(progress<100){
+		res.status(200).json({code:1003,msg:progress});
+	}else{
+		res.status(200).json({code:1004,msg:progress});
+	}
+});
+
 
 module.exports=router;
